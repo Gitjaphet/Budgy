e6221075d7946f4b58e20116ce3e4df3de9c7c7c
Japhet <japhetvaleureux@gmail.com>
2026-06-30 15:28:14 +0300

    fix: redirect login to login_public on public tenant

--- backend/tenants/urls.py
+++ backend/tenants/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('inscription/', views.inscription, name='inscription'),
-    path('login/', views.login_public, name='login_public'),
+    path('connexion/', views.login_public, name='login_public'),
 ]
